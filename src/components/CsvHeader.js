1b8476d330545d9612652b6d24ac870b05848a0a
Dylan Travis <dylan.travis23@outlook.com>
2021-10-26 15:57:18 -0700

    loading spinner and csv reult ui

--- src/components/CsvHeader.js
+++ src/components/CsvHeader.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FaCheckCircle } from "react-icons/fa";
 import "../css/CsvHeader.css";
+import LoadingSpinner from "./LoadingSpinner";
 
 const CsvHeader = ({ header }) => {
   return (
     <div className="csv-header">
-      <div className="csv-header-container">
+      <div className="csv-header-table">
         <h1>{header.name}</h1>
         {header.values.slice(0, 3).map((item, index) => {
           return (
@@ -15,13 +17,19 @@ const CsvHeader = ({ header }) => {
         })}
       </div>
       <div className="csv-results">
+        {/* {!header.headerMatch && <LoadingSpinner />} */}
         {header.headerMatch && header.headerMatch === true ? (
-          <h4>Matched</h4>
+          <div className="results-header">
+            {" "}
+            <FaCheckCircle />
+            <h1>Matched</h1>
+          </div>
         ) : header.headerMatch === false ? (
           <h4>Failed </h4>
         ) : (
           ""
         )}
+        <div></div>
       </div>
     </div>
   );
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../css/LoadingSpinner.css";
+const LoadingSpinner = () => {
+  return <div className="ids-default"></div>;
+};
+
+export default LoadingSpinner;
